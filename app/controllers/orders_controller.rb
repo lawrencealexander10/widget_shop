3dc70ca96c7d809980b21b1d1ce6a8e31360883a
Alex Tenn <connection.l10@gmail.com>
2016-04-22 01:02:13 -0400

    fixed app?

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,30 @@
 class OrdersController < ApplicationController
-	def destroy
-  current_order.destroy
-  session[:order_id] = nil
-  redirect_to root_path, :notice => "Basket emptied successfully."
-end
 
-def checkout
-  @order = Shoppe::Order.find(current_order.id)
-end
+   before_filter(:except => :status) { redirect_to root_path unless has_order? }
 
-def checkout
-  @order = Shoppe::Order.find(current_order.id)
-  if request.patch?
-    if @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
-      redirect_to checkout_payment_path
+  def destroy
+    current_order.destroy
+    session[:order_id] = nil
+    respond_to do |wants|
+      wants.html { redirect_to root_path, :notice => "Your basket has been emptied successfully."}
+      wants.json do
+        flash[:notice] = "Your shopping bag is now empty."
+        render :json => {:status => 'complete', :redirect => root_path}
+      end
     end
   end
-end
+  
+  def checkout
+    @order = Shoppe::Order.find(current_order.id)
+    if request.patch?
+      if @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
+        redirect_to checkout_payment_path
+      end
+    end
+  end
 
 def payment
-  @order = Shoppe::Order.find(session[:current_order_id])
+  @order = Shoppe::Order.find(session[:order_id])
   if request.post?
     if @order.accept_stripe_token(params[:stripe_token])
       redirect_to checkout_confirmation_path
@@ -27,13 +32,69 @@ def payment
       flash.now[:notice] = "Could not exchange Stripe token. Please try again."
     end
   end
-end
+end 
 
-def confirmation
-  if request.post?
-    current_order.confirm!
-    session[:order_id] = nil
-    redirect_to root_path, :notice => "Order has been placed successfully!"
+  def confirmation
+    if request.post?
+      current_order.confirm!
+      session[:order_id] = nil
+      redirect_to root_path, :notice => "Order has been placed successfully!"
+    end
   end
-end
+
+  def remove_item
+      item = current_order.order_items.find(params[:order_item_id])
+      if current_order.order_items.count == 1
+        destroy
+      else
+        item.remove
+        respond_to do |wants|
+          wants.html { redirect_to request.referer, :notice => "Item has been removed from your basket successfully"}
+          wants.json do
+            current_order.reload
+            render :json => {:status => 'complete', :items => render_to_string(:partial => 'shared/order_items.html', :locals => {:order => current_order})}
+          end
+        end
+      end
+    end
+
+  def change_item_quantity
+    item = current_order.order_items.find(params[:order_item_id])
+    request.delete? ? item.decrease! : item.increase!
+    respond_to do |wants|
+      wants.html { redirect_to request.referer || root_path, :notice => "Quantity has been updated successfully." }
+      wants.json do
+        current_order.reload
+        if current_order.empty?
+          destroy
+        else
+          render :json => {:status => 'complete', :items => render_to_string(:partial => 'shared/order_items.html', :locals => {:order => current_order})}
+        end
+      end
+    end    
+    rescue Shoppe::Errors::NotEnoughStock => e
+    respond_to do |wants|
+      wants.html { redirect_to request.referer, :alert => "Unfortunately, we don't have enough stock. We only have #{e.available_stock} items available at the moment. Please get in touch though, we're always receiving new stock." }
+      wants.json { render :json => {:status => 'error', :message => "Unfortunateley, we don't have enough stock to add more items."} }
+    end
+  end
+
+    def change_delivery_service
+    if current_order.delivery_service = current_order.available_delivery_services.select { |s| s.id == params[:delivery_service].to_i}.first
+      current_order.save
+      respond_to do |wants|
+        wants.html { redirect_to request.referer, :notice => "Delivery service has been changed"}
+        wants.json do
+          current_order.reload
+          render :json => {:status => 'complete', :items => render_to_string(:partial => 'shared/order_items.html', :locals => {:order => current_order})}
+        end
+      end
+    else
+      respond_to do |wants|
+        wants.html { redirect_to request.referer, :alert => "You cannot select this delivery method."}
+        wants.json { render :json => {:status => 'error', :message => 'InvalidDeliveryMethod'}, :status => 422 }
+      end
+    end
+  end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,27 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Shoppe::Product.root.ordered.includes(:product_categories, :variants)
+    @products = Shoppe::Product.active.includes(:product_categories, :variants).root
     @products = @products.group_by(&:product_category)
   end
 
   def show
     @product = Shoppe::Product.root.find_by_permalink(params[:permalink])
+    @product_attributes = Shoppe::ProductAttribute.where(product_id: @product.id)
+    respond_to do |format|
+        format.html
+        format.json { render :json => {:product => @product, :attributes => @product_attributes}  }
+    end
   end
 
-
-def buy
-  @product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
-  current_order.order_items.add_item(@product, 1)
-  redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
-end
+  def buy
+    @product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
+    current_order.order_items.add_item(@product, 1)
+    redirect_to :back , :notice => "Product has been added successfuly!"
+      rescue Shoppe::Errors::NotEnoughStock => e
+    respond_to do |wants|
+      wants.html { redirect_to request.referer, :alert => "We're sorry but we don't have enough stock to add that many products. We currently have #{e.available_stock} item(s) in stock. Please try again."}
+      wants.json { render :json => {:error => 'NotEnoughStock', :available_stock => e.available_stock}}
+    end
+  end
 end
